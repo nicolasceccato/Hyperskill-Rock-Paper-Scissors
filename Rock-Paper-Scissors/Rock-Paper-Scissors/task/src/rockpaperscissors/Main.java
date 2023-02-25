dc7b4b5aa33c39cd12491fa3468a664b82f08ada
nicolasceccato <nicolasceccato@hotmail.com>
2023-02-24 21:26:19 -0300

    Agora o jogo fica em loop até o usuário digitar !exit

--- Rock-Paper-Scissors/Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
@@ -6,8 +6,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userOption = scanner.nextLine();
+        String userOption = "";
+        do {
 
+            userOption = scanner.next();
+            while ((!userOption.equals("rock")) && (!userOption.equals("paper")) && (!userOption.equals("scissors")) && (!userOption.equals("!exit"))) {
+                System.out.println("Invalid input");
+                userOption = scanner.nextLine();
+            }
+
+            if (userOption.equals("!exit")) {
+                break;
+            }
+
+            String computerOptionString = computerOption();
+
+
+            if (userOption.equals(computerOptionString)) {
+                System.out.println("There is a draw " + "(<" + computerOptionString + ">)");
+            } else if ((userOption.equals("rock") && computerOptionString.equals("scissors")) || (userOption.equals("scissors") && computerOptionString.equals("paper")) || (userOption.equals("paper") && computerOptionString.equals("rock"))) {
+                System.out.println("Well done. The computer chose " + computerOptionString + " and failed");
+            } else {
+                System.out.println("Sorry, but the computer chose " + computerOptionString);
+            }
+
+            userOption = scanner.nextLine();
+        } while (!userOption.equals("!exit"));
+
+        System.out.println("Bye!");
+
+    }
+
+    public static String computerOption() {
         Random random = new Random();
 
         int computerOption = random.nextInt(3);
@@ -27,13 +57,8 @@ public class Main {
                 break;
         }
 
+        return computerOptionString;
 
-        if (userOption.equals(computerOptionString)) {
-            System.out.println("There is a draw " + "(<" + computerOptionString + ">)");
-        } else if ((userOption.equals("rock") && computerOptionString.equals("scissors")) || (userOption.equals("scissors") && computerOptionString.equals("paper")) || (userOption.equals("paper") && computerOptionString.equals("rock"))) {
-            System.out.println("Well done. The computer chose " + computerOptionString + " and failed");
-        } else {
-            System.out.println("Sorry, but the computer chose " + computerOptionString);
-        }
+
     }
 }
